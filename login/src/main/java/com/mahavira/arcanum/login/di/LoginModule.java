c6170ba2471034e407a24266cd160553100c3bc4
Norman Lie <norman.lie@kudo.co.id>
2018-08-15 21:39:45 +0700

    add di for login module

---
+++ login/src/main/java/com/mahavira/arcanum/login/di/LoginModule.java
@@ -0,0 +1,14 @@
+package com.mahavira.arcanum.login.di;
+
+import dagger.Module;
+
+/**
+ * Created by norman on 13/07/18.
+ *
+ */
+
+@Module(includes = {LoginBuilderModule.class})
+public class LoginModule {
+
+
+}
