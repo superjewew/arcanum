a1c0a27f627394e7d4f1d9fbd19d27e63d742b80
Norman Lie <norman.lie@kudo.co.id>
2018-08-15 23:03:59 +0700

    continue develop login feature

--- login/src/main/java/com/mahavira/arcanum/login/presentation/feature/LoginActivity.java
+++ login/src/main/java/com/mahavira/arcanum/login/presentation/feature/LoginActivity.java
@@ -1,10 +1,11 @@
 package com.mahavira.arcanum.login.presentation.feature;
 
 import android.os.Bundle;
-
+import android.widget.Toast;
 import com.android.databinding.library.baseAdapters.BR;
 import com.mahavira.arcanum.login.R;
 import com.mahavira.arcanum.login.databinding.ActivityLoginBinding;
+import com.mahavira.arcanum.login.domain.entity.AuthParam;
 import com.mahavira.base.presentation.BaseActivity;
 
 public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewModel> {
@@ -22,5 +23,22 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewM
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getViewModel().getLoginResultData().observe(this, authResultResource -> {
+            if (authResultResource != null) {
+                switch (authResultResource.status) {
+                    case SUCCESS:
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        break;
+                    case ERROR:
+                        Toast.makeText(this, "Login Failed, " + authResultResource.message, Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                }
+            }
+        });
+
+        getDataBinding().setParam(new AuthParam("", ""));
+
     }
 }
--- login/src/main/java/com/mahavira/arcanum/login/presentation/feature/LoginViewModel.java
+++ login/src/main/java/com/mahavira/arcanum/login/presentation/feature/LoginViewModel.java
@@ -1,7 +1,13 @@
 package com.mahavira.arcanum.login.presentation.feature;
 
+import android.arch.lifecycle.MutableLiveData;
+import com.google.firebase.auth.AuthResult;
+import com.mahavira.arcanum.login.domain.entity.AuthParam;
+import com.mahavira.arcanum.login.domain.usecase.LoginUseCase;
+import com.mahavira.base.core.Resource;
 import com.mahavira.base.presentation.BaseViewModel;
-
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import javax.inject.Inject;
 
 /**
@@ -10,8 +16,35 @@ import javax.inject.Inject;
 
 public class LoginViewModel extends BaseViewModel {
 
-    @Inject
-    public LoginViewModel() {
+    private final MutableLiveData<Resource<AuthResult>> mLoginResultData = new MutableLiveData<>();
 
+    private LoginUseCase mLoginUseCase;
+
+    @Inject
+    public LoginViewModel(LoginUseCase loginUseCase) {
+        mLoginUseCase = loginUseCase;
+    }
+
+    public MutableLiveData<Resource<AuthResult>> getLoginResultData() {
+        return mLoginResultData;
+    }
+
+    public void attemptLogin(AuthParam param) {
+        mLoginUseCase.execute(param)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(__ -> showLoading())
+                .subscribe(this::onLoginSuccess, this::onLoginFailed);
+
+    }
+
+    private void onLoginSuccess(AuthResult result) {
+        hideLoading();
+        mLoginResultData.setValue(Resource.success(result));
+    }
+
+    private void onLoginFailed(Throwable throwable) {
+        hideLoading();
+        mLoginResultData.setValue(Resource.error(null, throwable.getLocalizedMessage(), null));
     }
 }
