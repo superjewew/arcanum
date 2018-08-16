fa82793f6cd87a6e142be1ea5cdfb30e77e37f81
Norman Lie <norman.lie@kudo.co.id>
2018-08-16 14:23:00 +0700

    add get store repo implementation

--- store/src/main/java/com/mahavira/arcanum/store/data/StoreRepoImpl.java
+++ store/src/main/java/com/mahavira/arcanum/store/data/StoreRepoImpl.java
@@ -1,8 +1,11 @@
 package com.mahavira.arcanum.store.data;
 
+import android.support.annotation.NonNull;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.mahavira.arcanum.store.domain.entity.Store;
 import com.mahavira.arcanum.store.domain.repo.StoreRepository;
+import io.reactivex.Maybe;
 import io.reactivex.Single;
 import java.util.List;
 import javax.inject.Inject;
@@ -13,6 +16,8 @@ import javax.inject.Inject;
 
 public class StoreRepoImpl implements StoreRepository {
 
+    private static final String STORE_COLLECTION = "partner";
+
     private FirebaseFirestore mInstance;
 
     @Inject
@@ -22,6 +27,14 @@ public class StoreRepoImpl implements StoreRepository {
 
     @Override
     public Single<List<Store>> getStore() {
-        return null;
+        return getValue(mInstance.collection(STORE_COLLECTION), Store.class).toSingle();
+    }
+
+    @NonNull
+    private <T> Maybe<List<T>> getValue(@NonNull final CollectionReference ref, Class<T> clazz) {
+        return Maybe.create(
+                e -> ref.get()
+                        .addOnCompleteListener(task -> e.onSuccess(task.getResult().toObjects(clazz)))
+                        .addOnFailureListener(e::onError));
     }
 }
