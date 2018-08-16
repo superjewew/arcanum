539fe0b8a31d66dd1131ce095f53c0aae4fd0193
Norman Lie <norman.lie@kudo.co.id>
2018-08-16 14:40:12 +0700

    refactor commonly used method to base

--- store/src/main/java/com/mahavira/arcanum/store/data/StoreRepoImpl.java
+++ store/src/main/java/com/mahavira/arcanum/store/data/StoreRepoImpl.java
@@ -1,11 +1,9 @@
 package com.mahavira.arcanum.store.data;
 
-import android.support.annotation.NonNull;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.mahavira.arcanum.store.domain.entity.Store;
 import com.mahavira.arcanum.store.domain.repo.StoreRepository;
-import io.reactivex.Maybe;
+import com.mahavira.base.core.BaseRepository;
 import io.reactivex.Single;
 import java.util.List;
 import javax.inject.Inject;
@@ -14,7 +12,7 @@ import javax.inject.Inject;
  * Created by norman on 16/08/18.
  */
 
-public class StoreRepoImpl implements StoreRepository {
+public class StoreRepoImpl extends BaseRepository implements StoreRepository {
 
     private static final String STORE_COLLECTION = "partner";
 
@@ -30,11 +28,4 @@ public class StoreRepoImpl implements StoreRepository {
         return getValue(mInstance.collection(STORE_COLLECTION), Store.class).toSingle();
     }
 
-    @NonNull
-    private <T> Maybe<List<T>> getValue(@NonNull final CollectionReference ref, Class<T> clazz) {
-        return Maybe.create(
-                e -> ref.get()
-                        .addOnCompleteListener(task -> e.onSuccess(task.getResult().toObjects(clazz)))
-                        .addOnFailureListener(e::onError));
-    }
 }
