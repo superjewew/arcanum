d28062efbd6bc96f7da1a0802ee63142ae6697b9
Norman Lie <norman.lie@kudo.co.id>
2018-09-24 17:07:47 +0700

    add firebase user display name change request after registering

--- login/src/main/java/com/mahavira/arcanum/login/presentation/feature/register/RegisterActivity.java
+++ login/src/main/java/com/mahavira/arcanum/login/presentation/feature/register/RegisterActivity.java
@@ -2,14 +2,21 @@ package com.mahavira.arcanum.login.presentation.feature.register;
 
 import android.os.Bundle;
 import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.mahavira.arcanum.login.BR;
 import com.mahavira.arcanum.login.R;
 import com.mahavira.arcanum.login.databinding.ActivityRegisterBinding;
 import com.mahavira.arcanum.login.domain.entity.Pass;
 import com.mahavira.base.entity.User;
 import com.mahavira.base.presentation.BaseActivity;
+import javax.inject.Inject;
 
 public class RegisterActivity extends BaseActivity<ActivityRegisterBinding, RegisterViewModel> {
+
+    @Inject
+    FirebaseAuth mAuth;
 
     @Override
     public int getViewModelBindingVariable() {
@@ -26,9 +33,17 @@ public class RegisterActivity extends BaseActivity<ActivityRegisterBinding, Regi
         super.onCreate(savedInstanceState);
 
         getViewModel().getRegisterResultData().observe(this, result -> {
-            if(result != null) {
+            if (result != null) {
                 switch (result.status) {
                     case SUCCESS:
+                        assert result.data != null;
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if (user != null) {
+                            UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(result.data.getName()).build();
+                            user.updateProfile(request);
+                        }
+
                         Toast.makeText(this, "Register Successful", Toast.LENGTH_SHORT).show();
                         this.finish();
                         break;
